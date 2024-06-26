3e94c49e690027692df887c085c6a4ff4da161eb
Craig Sobande <craigsobande83@gmail.com>
2024-06-25 22:06:12 -0400

    Work Complete

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,50 @@
 {
     public class Program
     {
+        public static int Add( int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1 , decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2 , bool isTrue)
+        {
+            var sum = 0;
+            var response = "";
+            if (isTrue)
+
+            {
+                sum = num1 + num2;
+
+                if( sum == 1)
+                {
+                    response = $"{sum} dollar.";
+                }
+                else
+                {
+                    response = $"{sum} dollars.";
+                }
+
+            }
+
+            return response;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(0, 1 , true));
+
+            Console.WriteLine();
+
+            Console.WriteLine(Add(2, 3, true));
+
+            Console.WriteLine();
+
+            Console.WriteLine (Add (7 , 9, true));
         }
     }
 }
